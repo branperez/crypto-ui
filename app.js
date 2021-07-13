672731635a3dc5e58a4a5c291d13a86f97521957
Brandon Perez <bperez83525@gmail.com>
2021-07-13 12:23:49 +0300

    refactor api call

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ Parameters for the API are here:
 */
 const apiUrl = "https://rest.coinapi.io/v1/assets";
 const apiKey = "7AEDAEF9-DEC4-4CC7-B682-C7AB3A36CD93"; // Only 100 calls are allowed, so you will need to generate your own key for this.
-let jsonData;
-let trimmedData = [];
 
 // This will call the CoinAPI and store the returned JSON:
 const callAPI = async () => {
@@ -19,17 +17,17 @@ const callAPI = async () => {
          headers: {
         'Accept': 'application/json',
         'X-CoinAPI-Key': `${apiKey}`,
-    }
-});
-    jsonData = await response.json();
-    getNamePricePairs(jsonData);
-    sortData(trimmedData);
+        }
+    });
+    const jsonData = await response.json();
+    const trimmedData = getNamePricePairs(jsonData);
+    return sortData(trimmedData);
 }
 
 /*
 Event Listeners are established here:
 */
-btnTest.addEventListener("click", function(){
+btnTest.addEventListener("click", () => {
     callAPI()
 })
 
@@ -37,23 +35,28 @@ btnTest.addEventListener("click", function(){
 /*
 Functions are established here:
 */
-function performSearch(){
+const performSearch = () => {
     // Perform the search logic here - so far grabbed the value from the input box.
     let searchText = searchBox.value;
     console.log(`Search criteria taken from text box: ${searchText}`)
 }
 
-function getNamePricePairs(inputJson){
+const getNamePricePairs = (inputJson) => {
     // Iterates through the returned JSON and extracts the currency name, ID and its USD value:
-    for (let i = 0; i<inputJson.length; i++){
-        if ((inputJson[i].name != null || undefined) && (inputJson[i].price_usd != null || undefined) && (inputJson[i].asset_id != null || undefined) && inputJson[i].type_is_crypto === 1) {
-            trimmedData = [...trimmedData, [inputJson[i].asset_id, inputJson[i].name, Math.round(inputJson[i].price_usd)]]
+    return inputJson.reduce((accumulator, crypto) => {
+        const price_usd = parseFloat(Number(crypto.price_usd).toString()).toFixed(6);
+        const { name, asset_id, type_is_crypto } = crypto;
+
+        if (name && price_usd && price_usd !== "NaN" && asset_id && type_is_crypto === 1) {
+            accumulator = [...accumulator, { asset_id, name, price_usd }]
         }
-    }
+        return accumulator;
+    }, []);
 }
 
-function sortData(arr){
+const sortData = (arr) => {
     // Sorts descending price USD:
-   arr.sort(function(a,b) {return b[2]-a[2]})
-   console.log(arr)
+   const sortedArray = arr.sort((a,b) => {return Number(b.price_usd) - Number(a.price_usd)})
+   console.log(sortedArray);
+   return sortedArray;
 }
